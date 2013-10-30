e68c62de7910b260222dca69f05357edacc2fedd
Chris Ramroth <Chris.Ramroth (at) gmx.de>
2013-10-30 17:09:46 +0100

    Klasse Kunde 2.

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Kunde.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Kunde.java
@@ -1,8 +1,90 @@
 package de.shop.kundenverwaltung.domain;
 
 public class Kunde {
-private Long id;
-private String nachname;
-private String vorname;
+	private Long id;
+	private String nachname;
+	private String vorname;
+	//TODO Adresse sobald die Klasse angelegt ist
+	private String email;
+	public Kunde(Long id, String nachname, String vorname, String email) {
+		super();
+		this.id = id;
+		this.nachname = nachname;
+		this.vorname = vorname;
+		this.email = email;
+	}
+	//TODO private List<Bestellung> bestellungen;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNachname() {
+		return nachname;
+	}
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((nachname == null) ? 0 : nachname.hashCode());
+		result = prime * result + ((vorname == null) ? 0 : vorname.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kunde other = (Kunde) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nachname == null) {
+			if (other.nachname != null)
+				return false;
+		} else if (!nachname.equals(other.nachname))
+			return false;
+		if (vorname == null) {
+			if (other.vorname != null)
+				return false;
+		} else if (!vorname.equals(other.vorname))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Kunde [id=" + id + ", nachname=" + nachname + ", vorname="
+				+ vorname + ", email=" + email + "]";
+	}
+	
 
 }
