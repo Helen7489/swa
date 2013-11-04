645e4ea63c8a6425c64c7d086ab38978948bad82
Chris Ramroth <Chris.Ramroth (at) gmx.de>
2013-11-04 19:25:08 +0100

    Mock ueberarbeitet

--- shop/src/main/java/de/shop/util/Mock.java
+++ shop/src/main/java/de/shop/util/Mock.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import de.shop.bestellverwaltung.domain.Bestellung;
 import de.shop.kundenverwaltung.domain.Kunde;
 
+
 /**
  * Emulation des Anwendungskerns
  */
@@ -19,57 +20,48 @@ public final class Mock {
 
 	// TODO Methoden anpassen
 	
-	/*
+	
 	public static Kunde findKundeById(Long id) {
 		if (id > MAX_ID) {
 			return null;
 		}
-		
-		final AbstractKunde kunde = id % 2 == 1 ? new Privatkunde() : new Firmenkunde();
+		final Kunde kunde = new Kunde();
 		kunde.setId(id);
 		kunde.setNachname("Nachname" + id);
+		kunde.setVorname("Vorname" + id);
 		kunde.setEmail("" + id + "@hska.de");
+		//TODO Klasse Adresse
 		
 		final Adresse adresse = new Adresse();
-		adresse.setId(id + 1);        // andere ID fuer die Adresse
+		adresse.setId(id + 1);        
 		adresse.setPlz("12345");
 		adresse.setOrt("Testort");
 		adresse.setKunde(kunde);
 		kunde.setAdresse(adresse);
 		
-		if (kunde.getClass().equals(Privatkunde.class)) {
-			final Privatkunde privatkunde = (Privatkunde) kunde;
-			final Set<HobbyType> hobbies = new HashSet<>();
-			hobbies.add(HobbyType.LESEN);
-			hobbies.add(HobbyType.REISEN);
-			privatkunde.setHobbies(hobbies);
-		}
-		
 		return kunde;
 	}
 
-	public static List<AbstractKunde> findAllKunden() {
+	public static List<Kunde> findAllKunden() {
 		final int anzahl = MAX_KUNDEN;
-		final List<AbstractKunde> kunden = new ArrayList<>(anzahl);
+		final List<Kunde> kunden = new ArrayList<>(anzahl);
 		for (int i = 1; i <= anzahl; i++) {
-			final AbstractKunde kunde = findKundeById(Long.valueOf(i));
+			final Kunde kunde = findKundeById(Long.valueOf(i));
 			kunden.add(kunde);			
 		}
 		return kunden;
 	}
 
-	public static List<AbstractKunde> findKundenByNachname(String nachname) {
+	public static List<Kunde> findKundenByNachname(String nachname) {
 		final int anzahl = nachname.length();
-		final List<AbstractKunde> kunden = new ArrayList<>(anzahl);
+		final List<Kunde> kunden = new ArrayList<>(anzahl);
 		for (int i = 1; i <= anzahl; i++) {
-			final AbstractKunde kunde = findKundeById(Long.valueOf(i));
+			final Kunde kunde = findKundeById(Long.valueOf(i));
 			kunde.setNachname(nachname);
 			kunden.add(kunde);			
 		}
 		return kunden;
 	}
-
-	*/
 	
 	// Bestellung suchen mit ID
 	public static Bestellung findBestellungById(Long id) {
@@ -101,30 +93,30 @@ public final class Mock {
 		
 		return bestellung;	
 	}
-	/*
-	public static AbstractKunde createKunde(AbstractKunde kunde) {
+	
+	public static Kunde createKunde(Kunde kunde) {
 		// Neue IDs fuer Kunde und zugehoerige Adresse
 		// Ein neuer Kunde hat auch keine Bestellungen
-		 
 		final String nachname = kunde.getNachname();
 		kunde.setId(Long.valueOf(nachname.length()));
+		
 		final Adresse adresse = kunde.getAdresse();
 		adresse.setId((Long.valueOf(nachname.length())) + 1);
 		adresse.setKunde(kunde);
+		
 		kunde.setBestellungen(null);
 		
 		System.out.println("Neuer Kunde: " + kunde);
 		return kunde;
 	}
 
-	public static void updateKunde(AbstractKunde kunde) {
+	public static void updateKunde(Kunde kunde) {
 		System.out.println("Aktualisierter Kunde: " + kunde);
 	}
 
 	public static void deleteKunde(Long kundeId) {
 		System.out.println("Kunde mit ID=" + kundeId + " geloescht");
 	}
-	*/
 	
 	private Mock() { /**/ } 
 }
