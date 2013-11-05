7e1e34dfb2fe8544ff8f31b1390c9b058b9578d9
Hendrik Weidner <wehe1013 (at) hs-karlsruhe.de>
2013-11-05 14:45:18 +0100

    jetzt auch mit site

---
+++ shop/src/main/java/de/shop/bestellverwaltung/service/BestellungServiceMock.java
@@ -0,0 +1,59 @@
+package de.shop.bestellverwaltung.service;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import de.shop.artikelverwaltung.service.ArtikelServiceMock;
+import de.shop.bestellverwaltung.domain.Bestellposition;
+import de.shop.bestellverwaltung.domain.Bestellung;
+import de.shop.kundenverwaltung.domain.Kunde;
+import de.shop.kundenverwaltung.service.KundeServiceMock;
+
+public class BestellungServiceMock {
+
+	private static final int MAX_BESTELLUNGEN = 4;
+
+	public static Bestellung findBestellungById(Long id) {
+
+		Bestellposition bp1 = new Bestellposition(ArtikelServiceMock.findArtikelById((long) 10));
+		Bestellposition bp2 = new Bestellposition(ArtikelServiceMock.findArtikelById((long) 20));
+		final Kunde kunde = KundeServiceMock.findKundeById(id + 1);
+
+		List<Bestellposition> liste = Arrays.asList(bp1, bp2);
+
+		final Bestellung bestellung = new Bestellung(kunde, liste);
+		bestellung.setId(id);
+		// bestellung.setAusgeliefert(false);
+		bestellung.setKunde(kunde);
+
+		return bestellung;
+	}
+
+	public static List<Bestellung> findBestellungenByKunde(Kunde kunde) {
+		// Beziehungsgeflecht zwischen Kunde und Bestellungen aufbauen
+		final int anzahl = kunde.getId().intValue() % MAX_BESTELLUNGEN + 1;
+		final List<Bestellung> bestellungen = new ArrayList<>(anzahl);
+		for (int i = 1; i <= anzahl; i++) {
+			final Bestellung bestellung = findBestellungById(Long.valueOf(i));
+			bestellung.setKunde(kunde);
+			bestellungen.add(bestellung);
+		}
+		kunde.setBestellungen(bestellungen);
+
+		return bestellungen;
+	}
+
+	// FIXME geht noch nicht
+	public static Bestellung createBestellung(Bestellung bestellung) {
+		final Long id = bestellung.getId();
+		bestellung.setId(Long.valueOf(id));
+
+		final URI KundeUri = bestellung.getKundeUri();
+		bestellung.setKundeUri(KundeUri);
+
+		return bestellung;
+	}
+
+}
---
+++ shop/src/main/java/de/shop/kundenverwaltung/service/KundeServiceMock.java
@@ -0,0 +1,61 @@
+package de.shop.kundenverwaltung.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.shop.kundenverwaltung.domain.Adresse;
+import de.shop.kundenverwaltung.domain.Kunde;
+
+public class KundeServiceMock {
+
+	private static final int MAX_KUNDEN = 8;
+
+	public static Kunde findKundeById(Long id) {
+
+		final Kunde kunde = new Kunde();
+		kunde.setId(id);
+		kunde.setNachname("Nachname" + id);
+		kunde.setVorname("Vorname" + id);
+		kunde.setEmail("" + id + "@hska.de");
+
+		final Adresse adresse = new Adresse();
+		adresse.setId(id + 1);
+		adresse.setPlz("12345");
+		adresse.setOrt("Testort");
+		adresse.setKunde(kunde);
+		kunde.setAdresse(adresse);
+
+		return kunde;
+	}
+
+	public static List<Kunde> findAllKunden() {
+		final int anzahl = MAX_KUNDEN;
+		final List<Kunde> kunden = new ArrayList<>(anzahl);
+		for (int i = 1; i <= anzahl; i++) {
+			final Kunde kunde = findKundeById(Long.valueOf(i));
+			kunden.add(kunde);
+		}
+		return kunden;
+	}
+
+	public static Kunde createKunde(Kunde kunde) {
+		// Neue IDs fuer Kunde und zugehoerige Adresse
+		// Ein neuer Kunde hat auch keine Bestellungen
+		final String nachname = kunde.getNachname();
+		kunde.setId(Long.valueOf(nachname.length()));
+
+		final Adresse adresse = kunde.getAdresse();
+		adresse.setId((Long.valueOf(nachname.length())) + 1);
+		adresse.setKunde(kunde);
+
+		kunde.setBestellungen(null);
+
+		System.out.println("Neuer Kunde: " + kunde);
+		return kunde;
+	}
+
+	public static void updateKunde(Kunde kunde) {
+		System.out.println("Aktualisierter Kunde: " + kunde);
+	}
+
+}
